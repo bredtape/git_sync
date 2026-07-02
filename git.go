4f5ab271be293e23fe231ab5ab02cd98221ffd06
Daniel <12280220+bredtape@users.noreply.github.com>
2026-07-02 11:43:39 +0200

    minor formatting

--- git.go
+++ git.go
@@ -100,7 +100,7 @@ func (g *GIT) cloneRepoToLocalTemp() (*git.Worktree, error) {
 		slog.Warn("plain clone failed", "err", err,
 			"error type", fmt.Sprintf("%T", err),
 			"url", g.remoteRepo.URL, "branch", g.remoteRepo.Branch)
-		return nil, errors.Wrapf(err, "failed to clone repository %s for branch %s", g.remoteRepo.URL, g.remoteRepo.Branch)
+		return nil, errors.Wrapf(err, "failed to clone repository '%s' for branch '%s'", g.remoteRepo.URL, g.remoteRepo.Branch)
 	}
 
 	return local.Worktree()
